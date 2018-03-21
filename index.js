34da054ff54611753b4b191dae29b27e4359ff01
kim tran <icycleitaly@gmail.com>
2018-03-21 21:16:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+const array1 = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const array2 = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+function theBeatlesPlay(array1, array2){
+  var array = [];
+  for (var i=0; i<array.length-1; i++){
+    array.push(array1[i]+' plays '+array2[i]);
+    
+  }
+  return array;
+}
+
+function johnLennonFacts(array){
+  var newArr = [];
+  for (var i=0; i<array.length; i++){
+    newArr.push(array[i]+'!!!')
+  }
+  return newArr;
+}
+
+function iLoveTheBeatles(n){
+  var array = [];
+  do{
+    array.push('I love the Beatles')
+    n++
+  } while (n<15);
+  return array;
+}
