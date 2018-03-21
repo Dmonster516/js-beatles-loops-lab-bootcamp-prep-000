93b823381005d4c505bdbd2fbfc3892361ab1ac4
kim tran <icycleitaly@gmail.com>
2018-03-21 21:45:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const array1 = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const array2 = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
 function theBeatlesPlay(array1, array2){
   var array = [];
